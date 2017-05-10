6af8094e1876fef58c2af075717921191e8885fa
ligi <ligi@ligi.de>
2017-05-10 20:03:41 +0200

    Improve layout of workpackage item view and use images for status

--- app/src/main/java/org/ligi/kontinuumapp/WorkPackageViewHolder.kt
+++ app/src/main/java/org/ligi/kontinuumapp/WorkPackageViewHolder.kt
@@ -3,13 +3,29 @@ package org.ligi.kontinuumapp
 import android.support.v7.widget.RecyclerView
 import android.text.format.DateUtils
 import android.view.View
+import kontinuum.model.StageInfo
+import kontinuum.model.StageStatus
 import kontinuum.model.WorkPackage
+import kontinuum.model.WorkPackageStatus
 import kotlinx.android.synthetic.main.item_workpackage.view.*
 
+fun WorkPackage.isSuccess() = stageInfoList.fold(true, { b: Boolean, stageInfo: StageInfo -> b && stageInfo.status != StageStatus.ERROR })
 class WorkPackageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bind(workPackage: WorkPackage) {
         itemView.commit_msg_text.text = workPackage.branch + ":" + workPackage.commitMessage
-        itemView.status_text.text = workPackage.workPackageStatus.name
+
+
+        itemView.status_image.setImageResource(when (workPackage.workPackageStatus) {
+            WorkPackageStatus.PROCESSING ->
+                R.drawable.ic_build_black_24dp
+            WorkPackageStatus.PENDING ->
+                R.drawable.ic_hourglass_empty_black_24dp
+
+            WorkPackageStatus.FINISHED ->
+            if(workPackage.isSuccess()) R.drawable.ic_sentiment_satisfied_black_24dp else R.drawable.ic_sentiment_dissatisfied_black_24dp
+
+        })
+
         itemView.project_text.text = workPackage.project
 
         itemView.time_text.text = workPackage.epochSeconds.toTime()
